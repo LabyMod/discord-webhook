9e1d5503f77b2c525b3393ef1cbdf19185f81a32
scrummer <scrummer@gmx.ch>
2018-11-26 14:33:47 +0100

    Embed base class #2

---
+++ src/DiscordWebhook/Embed.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace DiscordWebhook;
+
+/**
+ * Class Embed
+ *
+ * @author Scrummer <scrummer@gmx.ch>
+ * @package DiscordWebhook
+ */
+class Embed
+{
+    /**
+     * Embed components
+     */
+    const COMPONENT_AUTHOR    = 'embed.component.author';
+    const COMPONENT_FIELD     = 'embed.component.field';
+    const COMPONENT_FOOTER    = 'embed.component.footer';
+    const COMPONENT_IMAGE     = 'embed.component.image';
+    const COMPONENT_PROVIDER  = 'embed.component.provider';
+    const COMPONENT_THUMBNAIL = 'embed.component.thumbnail';
+    const COMPONENT_VIDEO     = 'embed.component.video';
+
+    /**
+     * Embed colors
+     */
+    const COLOR_DEFAULT     = 0;
+    const COLOR_AQUA        = 1752220;
+    const COLOR_GREEN       = 3066993;
+    const COLOR_BLUE        = 3447003;
+    const COLOR_PURPLE      = 10181046;
+    const COLOR_GOLD        = 15844367;
+    const COLOR_ORANGE      = 15105570;
+    const COLOR_RED         = 15158332;
+    const COLOR_GREY        = 9807270;
+    const COLOR_DARKER_GREY = 8359053;
+    const COLOR_NAVY        = 3426654;
+    const COLOR_DARK_AQUA   = 1146986;
+    const COLOR_DARK_GREEN  = 2067276;
+    const COLOR_DARK_BLUE   = 2123412;
+    const COLOR_DARK_PURPLE = 7419530;
+    const COLOR_DARK_GOLD   = 12745742;
+    const COLOR_DARK_ORANGE = 11027200;
+    const COLOR_DARK_RED    = 10038562;
+    const COLOR_DARK_GREY   = 9936031;
+    const COLOR_LIGHT_GREY  = 12370112;
+    const COLOR_DARK_NAVY   = 2899536;
+}
